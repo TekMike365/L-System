b2423eadec08c1e3641a6a443ea6266f5d54b1ae
Michael Sklenka <miskosklenka@gmail.com>
2023-12-23 17:39:39 +0100

    it draws

--- src/window.py
+++ src/window.py
@@ -29,15 +29,15 @@ class Window:
 
     
     def draw_point(self, pos: Vec2, radius, color) -> None:
-        self._canvas.create_oval(int(pos.x - radius), int(-1 * (pos.y - radius)),
-                                 int(pos.x + radius), int(-1 * (pos.y + radius)),
+        self._canvas.create_oval(int(pos.x - radius), int(-1 * (pos.y - radius) + self.height),
+                                 int(pos.x + radius), int(-1 * (pos.y + radius) + self.height),
                                  fill=color, width=0)
 
 
     def draw_line(self, p1: Vec2, p2: Vec2, width, color) -> None:
-        self._canvas.create_line(int(p1.x), int(-1 * p1.y), int(p2.x), int(-1 * p2.y), width=width, fill=color)
+        self._canvas.create_line(int(p1.x), int(-1 * p1.y) + self.height, int(p2.x), int(-1 * p2.y) + self.height, width=width, fill=color)
 
 
     def draw_polygon(self, points, color):
-        self._canvas.create_polygon([(int(e.x), int(-1 * e.y)) for e in points], width=0, fill=color)
+        self._canvas.create_polygon([(int(e.x), int(-1 * e.y) + self.height) for e in points], width=0, fill=color)
 
