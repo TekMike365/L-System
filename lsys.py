e3fad8fc377fcae6c5c6b18ac3de405ea62d3e6a
Michael Sklenka <miskosklenka@gmail.com>
2023-12-24 14:19:27 +0100

    reworked error messages

--- lsys.py
+++ lsys.py
@@ -10,24 +10,47 @@ WND_WIDTH = int(640 * 1.5)
 WND_HEIGHT = int(480 * 1.5)
 
 
+def print_help():
+    print("usage: lsys.py <file.lsys> <options>")
+    print("options:")
+    print("  -c  int   complexity")
+    print("  -h  none  print this message")
+
+
+E_OK = 0
+E_WRONG_AMOUNT_OF_ARGS = 1
+E_NOT_A_FILE = 2
+E_CANT_OPEN_FILE = 3
+E_WRONG_OPTIONS = 4
+
+
+def get_error_msg(code) -> None:
+    if code == E_WRONG_AMOUNT_OF_ARGS:
+        print_help()
+        return "Expected at least 1 argument got none."
+    elif code == E_NOT_A_FILE:
+        return "File doesn't exist."
+    elif code == E_CANT_OPEN_FILE:
+        return "Couldn't open file."
+    elif code == E_WRONG_OPTIONS:
+        print_help()
+        return "Wrong options."
+
+
 def main(args):
     if len(args) < 2:
-        Log.error("Expected 1 argument got none.")
-        print("lsys.py <script_path>")
-        return 1
+        return E_WRONG_AMOUNT_OF_ARGS
 
     script_path = args[1]
 
     if not path.isfile(script_path):
-        Log.error(f"File '{script_path}' doesn't exist")
-        return 1
+        return E_NOT_A_FILE
 
     source = None
     with open(script_path, "r") as f: source = f.read()
 
     if source == None:
-        Log.error("Couldn't open file.")
-        return 1
+        return E_CANT_OPEN_FILE
 
     # command options
     complexity = 4
@@ -35,9 +58,13 @@ def main(args):
     if len(args) > 2:
         i = 2
         while i < len(args):
-            if args[i] == "-c": i += 1
+            if args[i] == "-h":
+                print_help()
+                return E_OK
+            elif args[i] == "-c":
+                i += 1
                 if i > len(args) - 1:
-                    return 1
+                    return E_WRONG_OPTIONS
                 complexity = int(args[i])
             i += 1
 
@@ -57,4 +84,6 @@ def main(args):
 
 if __name__ == "__main__":
     code = main(argv)
+    if code != 0:
+        Log.error(get_error_msg(code))
     print("Exited with exit code:", code)
